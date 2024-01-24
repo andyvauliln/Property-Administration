c632194b5efdccc7ed1c2857d7139e7373be4329
Andrei <andy.vaulin@gmail.com>
2024-01-25 02:13:00 +0800

    added cron task

--- cron.js
+++ cron.js
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 
 // Schedule task to run every day at 08:00
 cron.schedule('0 8 * * *', function () {
-    console.log('Running Django cron...');
+    console.log('Running Django telegram notification cron...');
     exec('/usr/bin/python3 /home/superuser/site/manage.py telegram_notifications', { cwd: '/home/superuser/site/' }, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing task: ${error}`);
@@ -13,4 +13,18 @@ cron.schedule('0 8 * * *', function () {
         console.log(`stderr: ${stderr}`);
     });
     console.log(`stderr: finished cron`);
+});
+
+// Schedule task to run every day at 08:00
+cron.schedule('0 12 * * *', function () {
+    console.log('Running Django sms cron...');
+    exec('/usr/bin/python3 /home/superuser/site/manage.py sms_notifications', { cwd: '/home/superuser/site/' }, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing task: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    });
+    console.log(`stderr: finished sms cron`);
 });
