b1862a997f72a6eccc92bdf3352ab52d712ef660
Andy Vaulin <andy.vaulin@gmail.com>
2024-05-30 17:37:41 +0000

    fixed production bugs

--- mysite/views/docusign.py
+++ mysite/views/docusign.py
@@ -7,12 +7,7 @@ from django.http import JsonResponse
 import logging
 from mysite.models import Booking
 
-logger_common = logging.getLogger('mysite.common')
 
-
-def print_info(message):
-    print(message)
-    logger_common.debug(message)
 
 def get_user(access_token):
     url = "https://account-d.docusign.com/oauth/userinfo"
@@ -26,14 +21,13 @@ def get_docusign_token():
     api_client = ApiClient()
     api_client.set_base_path(os.environ["DOCUSIGN_API_URL"])
     private_key_content = os.environ["DOCUSIGN_PRIVATE_KEY"]
-    if '\\n' in private_key_content:
-        private_key_content = private_key_content.replace('\\n', '\n')
+    private_key = private_key_content.replace("\\n", "\n")
 
     token = api_client.request_jwt_user_token(
         client_id=os.environ["DOCUSIGN_INTEGRATION_KEY"],
         user_id=os.environ["DOCUSIGN_USER_ID"],
         oauth_host_name=os.environ["DOCUSIGN_AUTH_SERVER"],
-        private_key_bytes=private_key_content.encode('utf-8'),
+        private_key_bytes=private_key.encode('utf-8'),
         expires_in=3600,
         scopes=["signature"]
     )
@@ -58,26 +52,19 @@ def create_api_client(access_token):
 
 
 def docusign_callback(request):
-    print_info(request)
     envelope_id = request.GET.get('envelopeId')
     secret_key = request.GET.get('secret_key')
-
-    print_info(envelope_id)
-    print_info(secret_key)
 
 
     try:
         # Initialize your DocuSign API client
         token = get_docusign_token()
-        print_info(token)
         api_client = create_api_client(token)
-        print_info("Create Client")
         envelopes_api = EnvelopesApi(api_client)
 
         # Fetch form data
         account_id = os.environ["DOCUSIGN_API_ACCOUNT_ID"]
         form_data_response = envelopes_api.get_form_data(account_id=account_id, envelope_id=envelope_id)
-        print_info(form_data_response)
 
         # Process and parse form data
         recipient_form_data = form_data_response.recipient_form_data[0] if form_data_response.recipient_form_data else None
@@ -85,19 +72,19 @@ def docusign_callback(request):
 
         # Create a dictionary from form data items
         form_data_dict = {item.name: item.value for item in form_data_items}
-        print_info( form_data_dict)
-        form_data_dict['contract_date'] = timezone.now().strftime('%m/%d/%Y')
         bookingid = int(form_data_dict['agreement_number'][2:-1])
         booking = Booking.objects.get(id=bookingid)
-        booking.status = 'Waiting Payment'
-        tenant = booking.tenant
-        tenant.full_name = form_data_dict['occupant_name']
-        tenant.email = form_data_dict['email']
-        tenant.phone = form_data_dict['phone']
-        tenant.save()
-        booking.save()
+        if booking:
+            booking.status = 'Waiting Payment'
+            booking.save()
+            tenant = booking.tenant
+            tenant.full_name = form_data_dict['occupant']
+            tenant.email = form_data_dict['email']
+            tenant.phone = form_data_dict['phone']
+            tenant.save()
+            booking.save()
 
         return JsonResponse({'status': 'success', 'formData': form_data_dict})
     except Exception as e:
-        print_info(e)
+        print(e)
         return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
